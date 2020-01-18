9cae16d0a260a1ce91837ebfac4d564c5060cc5b
lutz-harder <lutz.harder@freenet.ag>
2020-01-18 16:36:54 +0100

    more

--- demo/demo.go
+++ demo/demo.go
@@ -13,6 +13,21 @@ components {
     w: 1.0
   }
 }
+components {
+  id: "gui"
+  component: "/demo/demo.gui"
+  position {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
 embedded_components {
   id: "label"
   type: "label"
